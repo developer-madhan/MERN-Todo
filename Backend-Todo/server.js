1d5016d3ae945bfb12d9921bec5b56d348d5ea00
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-08-07 22:15:03 +0530

    install mongodb and connect ,create schema ,modal

--- Backend-Todo/server.js
+++ Backend-Todo/server.js
@@ -1,10 +1,11 @@
 // Express js
 // Using express
 const express = require("express");
+const mongoose = require("mongoose");
 
 // create an instance of express
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 // Define a route
 // Test is it working?
@@ -13,26 +14,44 @@ app.use(express.json())
 // });
 
 // sample in-memory stolrage for todo items
-let todos = [];
+// let todos = [];
+
+// connecting mongodb
+mongoose
+  .connect("mongodb://localhost:27017/todo-app")
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+//   create schema
+const todoSchema = new mongoose.Schema({
+    title:String,
+    description:String
+})
+
+// create modal
+const todoModal =mongoose.model('Todo',todoSchema);
 
 // Create a new todo item
-app.post('/todos',(req,res)=>{
-    const {title,description}= req.body;
-    const newTodo ={
-        id:todos.length +1,
-        title,
-        description
-    };
-    todos.push(newTodo);
-    console.log(todos);
-    res.status(201).json(newTodo);
-})
+app.post("/todos", (req, res) => {
+  const { title, description } = req.body;
+  const newTodo = {
+    id: todos.length + 1,
+    title,
+    description,
+  };
+  todos.push(newTodo);
+  console.log(todos);
+  res.status(201).json(newTodo);
+});
 
 // get all items
-app.get('/todos',(req,res)=>{
-    res.json(todos);
-})
-
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
 
 // start the server
 const port = 8000;
