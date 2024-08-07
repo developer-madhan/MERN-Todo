0d53b9d1d55e70b6bd7143261b10b6ea92cb2e24
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-08-07 21:55:58 +0530

    create api for todo items and api for get all

--- Backend-Todo/server.js
+++ Backend-Todo/server.js
@@ -1,1 +1,41 @@
 // Express js
+// Using express
+const express = require("express");
+
+// create an instance of express
+const app = express();
+app.use(express.json())
+
+// Define a route
+// Test is it working?
+// app.get("/", (req, res) => {
+//   res.send("hello world!");
+// });
+
+// sample in-memory stolrage for todo items
+let todos = [];
+
+// Create a new todo item
+app.post('/todos',(req,res)=>{
+    const {title,description}= req.body;
+    const newTodo ={
+        id:todos.length +1,
+        title,
+        description
+    };
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
+})
+
+// get all items
+app.get('/todos',(req,res)=>{
+    res.json(todos);
+})
+
+
+// start the server
+const port = 8000;
+app.listen(port, () => {
+  console.log("Server is listening to port" + port);
+});
